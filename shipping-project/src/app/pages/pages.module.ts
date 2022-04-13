d59d7844cee28d156f2f3f2584fc7b4342c434d4
deyandobrev <dobrev.de@gmail.com>
2022-04-13 17:55:14 +0300

    pages-routing

--- shipping-project/src/app/pages/pages.module.ts
+++ shipping-project/src/app/pages/pages.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AboutComponent],
-  imports: [CommonModule],
+  declarations: [AboutComponent, WelcomeComponent],
+  imports: [CommonModule, RouterModule],
   exports: [],
 })
 export class PagesModule {}
